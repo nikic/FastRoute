b467380cc37a6162b009d16d559300be777604df
Nikita Popov <nikic@php.net>
2014-02-18 17:53:32 +0100

    Handle METHOD_NOT_ALLOWED for static routes

--- src/Dispatcher/GroupCountBased.php
+++ src/Dispatcher/GroupCountBased.php
@@ -14,14 +14,23 @@ class GroupCountBased implements Dispatcher {
     }
 
     public function dispatch($httpMethod, $uri) {
-        if (isset($this->staticRoutes[$uri][$httpMethod])) {
-            return [self::FOUND, $this->staticRoutes[$uri][$httpMethod], []];
+        if (isset($this->staticRoutes[$uri])) {
+            return $this->dispatchStaticRoute($httpMethod, $uri);
+        } else {
+            return $this->dispatchVariableRoute($httpMethod, $uri);
         }
-
-        return $this->matchRoute($httpMethod, $uri);
     }
 
-    private function matchRoute($httpMethod, $uri) {
+    private function dispatchStaticRoute($httpMethod, $uri) {
+        $routes = $this->staticRoutes[$uri];
+        if (!isset($routes[$httpMethod])) {
+            return [self::METHOD_NOT_ALLOWED, array_keys($routes)];
+        }
+
+        return [self::FOUND, $routes[$httpMethod], []];
+    }
+
+    private function dispatchVariableRoute($httpMethod, $uri) {
         foreach ($this->regexes as $i => $regex) {
             if (!preg_match($regex, $uri, $matches)) {
                 continue;
