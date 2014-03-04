be4a6a1595ec3608a0e54629adf24bf58ac58686
Tyler Sommer <sommertm@gmail.com>
2014-03-04 03:12:11 -0800

    Added docblocks to Route and RouteCollector

--- src/RouteCollector.php
+++ src/RouteCollector.php
@@ -3,19 +3,44 @@
 namespace FastRoute;
 
 class RouteCollector {
+    /**
+     * @var RouteParser
+     */
     private $routeParser;
+
+    /**
+     * @var DataGenerator
+     */
     private $dataGenerator;
 
+    /**
+     * Constructor
+     * 
+     * @param RouteParser   $routeParser
+     * @param DataGenerator $dataGenerator
+     */
     public function __construct(RouteParser $routeParser, DataGenerator $dataGenerator) {
         $this->routeParser = $routeParser;
         $this->dataGenerator = $dataGenerator;
     }
 
-    function addRoute($httpMethod, $route, $handler) {
+    /**
+     * Add a route to the collection
+     * 
+     * @param string   $httpMethod
+     * @param string   $route
+     * @param callable $handler
+     */
+    public function addRoute($httpMethod, $route, $handler) {
         $routeData = $this->routeParser->parse($route);
         $this->dataGenerator->addRoute($httpMethod, $routeData, $handler);
     }
 
+    /**
+     * Get collected route data
+     * 
+     * @return array
+     */
     public function getData() {
         return $this->dataGenerator->getData();
     }
