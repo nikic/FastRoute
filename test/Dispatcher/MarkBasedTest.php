b3960cf3e83b017bea0a9fdda63de3f204b5a6f2
nikic <nikita.ppv@googlemail.com>
2014-09-30 18:50:49 +0200

    Make test for MARK support HHVM compatible

--- test/Dispatcher/MarkBasedTest.php
+++ test/Dispatcher/MarkBasedTest.php
@@ -4,7 +4,8 @@ namespace FastRoute\Dispatcher;
 
 class MarkBasedTest extends DispatcherTest {
     public function setUp() {
-        if (version_compare(PHP_VERSION, '5.6.0-beta1', '<')) {
+        preg_match('/(*MARK:A)a/', 'a', $matches);
+        if (!isset($matches['MARK'])) {
             $this->markTestSkipped('PHP 5.6 required for MARK support');
         }
     }
