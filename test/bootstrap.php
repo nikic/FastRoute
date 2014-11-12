1db94984bae4ee83eeb0632997d21d76192f299b
Daniel Bruce <dbruce@vectorface.com>
2014-11-12 16:29:19 -0500

    Change to require_once to prevent a PHP Fatal error during unit tests.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../src/functions.php';
+require_once __DIR__ . '/../src/functions.php';
 
 spl_autoload_register(function($class) {
     if (strpos($class, 'FastRoute\\') === 0) {
