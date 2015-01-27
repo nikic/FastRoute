0bd49322be5780c1a84264203b047fcd43fe79b2
Joao Gilberto Magalhaes <joao@byjg.com.br>
2015-01-27 01:19:20 -0200

    Throws an exception if the cache file is invalid

--- src/functions.php
+++ src/functions.php
@@ -43,9 +43,9 @@ function cachedDispatcher(callable $routeDefinitionCallback, array $options = []
 
     if (!$options['cacheDisabled'] && file_exists($options['cacheFile'])) {
         $dispatchData = require $options['cacheFile'];
-	if (!is_array($dispatchData)) {
-		throw new \Exception('Invalid Cache File "' . $options['cacheFile'] . '"');
-	}
+        if (!is_array($dispatchData)) {
+	    throw new \Exception('Invalid Cache File "' . $options['cacheFile'] . '"');
+        }
         return new $options['dispatcher']($dispatchData);
     }
 
