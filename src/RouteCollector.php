add8c6765c7cc2276812beca0e02141f5d807978
Toby Lawrence <toby@bluestatedigital.com>
2015-02-22 13:31:16 +0000

    Add convenience methods to RouteCollector to more easily specify routes.

--- src/RouteCollector.php
+++ src/RouteCollector.php
@@ -34,6 +34,79 @@ class RouteCollector {
     }
 
     /**
+     * Adds a GET route to the collection.
+     *
+     * The syntax used in the $route string depends on the used route parser.
+     *
+     * @param string $route
+     * @param mixed  $handler
+     */
+    public function get($route, $handler) {
+        $this->addRoute('GET', $route, $handler);
+    }
+
+    /**
+     * Adds a POST route to the collection.
+     *
+     * The syntax used in the $route string depends on the used route parser.
+     *
+     * @param string $route
+     * @param mixed  $handler
+     */
+    public function post($route, $handler) {
+        $this->addRoute('POST', $route, $handler);
+    }
+
+    /**
+     * Adds a PUT route to the collection.
+     *
+     * The syntax used in the $route string depends on the used route parser.
+     *
+     * @param string $route
+     * @param mixed  $handler
+     */
+    public function put($route, $handler) {
+        $this->addRoute('PUT', $route, $handler);
+    }
+
+    /**
+     * Adds a DELETE route to the collection.
+     *
+     * The syntax used in the $route string depends on the used route parser.
+     *
+     * @param string $route
+     * @param mixed  $handler
+     */
+    public function delete($route, $handler) {
+        $this->addRoute('DELETE', $route, $handler);
+    }
+
+    /**
+     * Adds a HEAD route to the collection.
+     *
+     * The syntax used in the $route string depends on the used route parser.
+     *
+     * @param string $route
+     * @param mixed  $handler
+     */
+    public function head($route, $handler) {
+        $this->addRoute('HEAD', $route, $handler);
+    }
+
+    /**
+     * Adds an ANY route to the collection.  This route matches all HTTP methods.
+     *
+     * The syntax used in the $route string depends on the used route parser.
+     *
+     * @param string $route
+     * @param mixed  $handler
+     */
+    public function any($route, $handler) {
+        $this->addRoute('ANY', $route, $handler);
+    }
+
+
+    /**
      * Returns the collected route data, as provided by the data generator.
      *
      * @return array
