860569e0fa0fd38ea0b231b89e6045176bd79349
Luís Cobucci <lcobucci@gmail.com>
2019-06-08 14:40:15 +0200

    Disallow yoda conditions

--- src/DataGenerator/RegexBasedAbstract.php
+++ src/DataGenerator/RegexBasedAbstract.php
@@ -184,7 +184,7 @@ abstract class RegexBasedAbstract implements DataGenerator
 
     private function regexHasCapturingGroups(string $regex): bool
     {
-        if (false === strpos($regex, '(')) {
+        if (strpos($regex, '(') === false) {
             // Needs to have at least a ( to contain a capturing group
             return false;
         }
