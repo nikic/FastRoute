0a1b06305a6565cff134b2d1ae3d86fa2c78ac51
TheKito <11139894+TheKito@users.noreply.github.com>
2020-03-01 12:39:04 -0300

    remove unused use functions

--- src/functions.php
+++ src/functions.php
@@ -3,14 +3,7 @@ declare(strict_types=1);
 
 namespace FastRoute;
 
-use LogicException;
-use RuntimeException;
-use function assert;
-use function file_exists;
-use function file_put_contents;
 use function function_exists;
-use function is_array;
-use function var_export;
 
 if (! function_exists('FastRoute\simpleDispatcher')) {
     /**
