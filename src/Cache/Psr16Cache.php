ebff3bba913e846d9fad7d270fb1b7fd987b2c40
Luís Cobucci <lcobucci@gmail.com>
2024-01-30 22:05:28 +0100

    Provide PSR-16 adapter for cache
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

---
+++ src/Cache/Psr16Cache.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace FastRoute\Cache;
+
+use FastRoute\Cache;
+use FastRoute\DataGenerator;
+use Psr\SimpleCache\CacheInterface;
+
+use function is_array;
+
+/** @phpstan-import-type RouteData from DataGenerator */
+final class Psr16Cache implements Cache
+{
+    public function __construct(private readonly CacheInterface $cache)
+    {
+    }
+
+    /** @inheritDoc */
+    public function get(string $key, callable $loader): array
+    {
+        $result = $this->cache->get($key);
+
+        if (is_array($result)) {
+            // @phpstan-ignore-next-line because we won´t be able to validate the array shape in a performant way
+            return $result;
+        }
+
+        $data = $loader();
+        $this->cache->set($key, $data);
+
+        return $data;
+    }
+}
