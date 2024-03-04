c7877eeb9e180b47be9a9b2786880cfedeeea2a1
Luís Cobucci <lcobucci@gmail.com>
2024-03-04 20:56:06 +0100

    Implement component to generate URIs from named routes
    This introduces the ability to create URIs from the configured routes,
    following the same semantics of route dispatching when it comes to URL
    encoding (it needs to be handled by the user of the library).
    
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

---
+++ src/GenerateUri/FromProcessedConfiguration.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace FastRoute\GenerateUri;
+
+use FastRoute\GenerateUri;
+use FastRoute\RouteParser;
+
+use function array_key_exists;
+use function array_keys;
+use function assert;
+use function count;
+use function is_string;
+use function preg_match;
+
+/**
+ * @phpstan-import-type RoutesForUriGeneration from GenerateUri
+ * @phpstan-import-type UriSubstitutions from GenerateUri
+ * @phpstan-import-type ParsedRoute from RouteParser
+ */
+final class FromProcessedConfiguration implements GenerateUri
+{
+    /** @param RoutesForUriGeneration $processedConfiguration */
+    public function __construct(private readonly array $processedConfiguration)
+    {
+    }
+
+    /** @inheritDoc */
+    public function forRoute(string $name, array $substitutions = []): string
+    {
+        if (! array_key_exists($name, $this->processedConfiguration)) {
+            throw UriCouldNotBeGenerated::routeIsUndefined($name);
+        }
+
+        $missingParameters = [];
+
+        foreach ($this->processedConfiguration[$name] as $parsedRoute) {
+            $missingParameters = $this->missingParameters($parsedRoute, $substitutions);
+
+            // Only attempt to generate the path if we have the necessary info
+            if (count($missingParameters) === 0) {
+                return $this->generatePath($name, $parsedRoute, $substitutions);
+            }
+        }
+
+        assert(count($missingParameters) > 0);
+
+        throw UriCouldNotBeGenerated::insufficientParameters(
+            $name,
+            $missingParameters,
+            array_keys($substitutions),
+        );
+    }
+
+    /**
+     * Returns the expected parameters that were not passed as substitutions
+     *
+     * @param ParsedRoute      $parts
+     * @param UriSubstitutions $substitutions
+     *
+     * @return list<string>
+     */
+    private function missingParameters(array $parts, array $substitutions): array
+    {
+        $missingParameters = [];
+
+        foreach ($parts as $part) {
+            if (is_string($part) || array_key_exists($part[0], $substitutions)) {
+                continue;
+            }
+
+            $missingParameters[] = $part[0];
+        }
+
+        return $missingParameters;
+    }
+
+    /**
+     * @param ParsedRoute      $parsedRoute
+     * @param UriSubstitutions $substitutions
+     */
+    private function generatePath(string $route, array $parsedRoute, array $substitutions): string
+    {
+        $path = '';
+
+        foreach ($parsedRoute as $part) {
+            if (is_string($part)) {
+                $path .= $part;
+
+                continue;
+            }
+
+            [$parameterName, $regex] = $part;
+
+            if (preg_match('~^' . $regex . '$~u', $substitutions[$parameterName]) !== 1) {
+                throw UriCouldNotBeGenerated::parameterDoesNotMatchThePattern($route, $parameterName, $regex);
+            }
+
+            $path .= $substitutions[$parameterName];
+        }
+
+        return $path;
+    }
+}
